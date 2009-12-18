2fa37089aa6d39641234a363cb452b9b9ad96a9b
antiraum <antiraum@13490c86-dd94-11de-8752-9f8e91036772>
2009-12-18 12:59:38 +0000

    added log4j

--- src/it/unitn/disi/aose/firerespsim/FireAgent.java
+++ src/it/unitn/disi/aose/firerespsim/FireAgent.java
@@ -1,12 +1,18 @@
 package it.unitn.disi.aose.firerespsim;
 
 import jade.core.Agent;
+import org.apache.log4j.Logger;
 
 /**
  * @author tom
  */
 @SuppressWarnings("serial")
 public final class FireAgent extends Agent {
+    
+    /**
+     * Package scoped for faster access by inner classes.
+     */
+    static Logger logger = Logger.getLogger("it.unitn.disi.aose.firerespsim");
     
     /**
      * @see jade.core.Agent#setup()
