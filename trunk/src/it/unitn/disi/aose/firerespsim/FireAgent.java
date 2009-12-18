b0e77c91a1a0b5e33cf986128a647b90f34da6b1
antiraum <antiraum@13490c86-dd94-11de-8752-9f8e91036772>
2009-12-18 10:48:34 +0000

    initial commit

---
+++ trunk/src/it/unitn/disi/aose/firerespsim/FireAgent.java
@@ -0,0 +1,19 @@
+package it.unitn.disi.aose.firerespsim;
+
+import jade.core.Agent;
+
+/**
+ * @author tom
+ */
+@SuppressWarnings("serial")
+public final class FireAgent extends Agent {
+    
+    /**
+     * @see jade.core.Agent#setup()
+     */
+    @Override
+    protected void setup() {
+
+        super.setup();
+    }
+}
