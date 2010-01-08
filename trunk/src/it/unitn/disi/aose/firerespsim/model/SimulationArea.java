9bb1848ced30a41d4bd3794f6be72a9427880eed
antiraum <antiraum@13490c86-dd94-11de-8752-9f8e91036772>
2010-01-08 22:01:22 +0000

    added distance methods to area model

--- trunk/src/it/unitn/disi/aose/firerespsim/model/SimulationArea.java
+++ trunk/src/it/unitn/disi/aose/firerespsim/model/SimulationArea.java
@@ -1,5 +1,7 @@
 package it.unitn.disi.aose.firerespsim.model;
 
+import it.unitn.disi.aose.firerespsim.agents.VehicleAgent;
+
 /**
  * Represents the simulation area.
  * 
@@ -69,5 +71,33 @@ public final class SimulationArea {
     public String toString() {
 
         return width + FIELD_SEPARATOR + height;
+    }
+
+    /**
+     * Calculates the distance of two positions on the simulation area in number of moves necessary for a
+     * {@link VehicleAgent} to cross.
+     * 
+     * @param position1
+     * @param position2
+     * @return Number of moves to cross.
+     */
+    public static final int getDistance(final Position position1, final Position position2) {
+    
+        return getDistance(position1.getRow(), position1.getCol(), position2.getRow(), position2.getCol());
+    }
+
+    /**
+     * Calculates the distance of two points on the simulation area in number of moves necessary for a
+     * {@link VehicleAgent} to cross.
+     * 
+     * @param row1
+     * @param col1
+     * @param row2
+     * @param col2
+     * @return Number of moves to cross.
+     */
+    public static final int getDistance(final int row1, final int col1, final int row2, final int col2) {
+    
+        return Math.max(Math.abs(Math.abs(row1) - Math.abs(row2)), Math.abs(Math.abs(col1) - Math.abs(col2)));
     }
 }
